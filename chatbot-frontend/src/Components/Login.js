37f1e7045596a6fefbff90ff6971625a8cf29d2d
Revanth L <85238812+Revanth-1707@users.noreply.github.com>
2024-03-26 16:09:59 +0530

    Update Login.js

--- chatbot-frontend/src/Components/Login.js
+++ chatbot-frontend/src/Components/Login.js
@@ -78,7 +78,7 @@ const Login = ({ setIsAuthenticated, isAuthenticated}) => {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post('https://agri-chatbot-cyan.vercel.app/api/login', { email, password });
+      const response = await axios.post('https://agri-chatbot-cyan.vercel.app/login', { email, password });
       console.log(response.data);
       setIsAuthenticated(true);
       localStorage.setItem('isAuthenticated', 'true');
